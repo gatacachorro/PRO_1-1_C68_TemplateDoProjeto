e46d6de33fcdf996cb8c62322b2af4f9d609287c
gatacachorro <100207223+gatacachorro@users.noreply.github.com>
2023-07-03 18:58:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,10 +4,8 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 export default class App extends Component {
   render() {
 
-    //return <BottomTabNavigator>;
-    //<BottomTabNavigator/>
-    //return <BottomTabNavigator />;
-    //return <BottomTabNavigator <BottomTabNavigator> />;
-
+   
+    return <BottomTabNavigator />;
+    
   }
 }
